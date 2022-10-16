5e06dda856283254c8938df824268ce0753699f8
Shafeeq <sshafeeq4526@gmail.com>
2022-10-16 08:11:42 -0700

    commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Form from './Components/Form';
+import Header from './Components/Header';
+import TodosList from './Components/TodosList';
+
 
 function App() {
+
+  const [input,setInput]= useState("");
+  const [todos,setTodos]= useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className='app-wrapper'>
+          <div>
+          <Header/>
+          </div>
+          <div>
+            <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            />
+          </div>
+          <div>
+            <TodosList todos={todos} setTodos={setTodos}/>
+          </div>
+        </div>
     </div>
   );
 }
